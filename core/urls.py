721752ad8cedd2556838bb2bc34bc1fefe345e9f
Arthur._.Kings <arthurogembo@gmail.com>
2023-08-06 12:19:06 +0300

    first commit

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import item_list
+from .views import item_list, fashion, electronics, jewellery
 app_name = 'core'
 
 urlpatterns = [
-    path('', item_list, name='item-list')
+    path('', item_list, name='item-list'),
+    path('fashion/', fashion, name='fashion'),
+    path('electronics/', electronics, name='electronics'),
+    path('jewellery/', jewellery, name='jewellery'),
+    
 ]
 
 
--- core/views.py
+++ core/views.py
@@ -4,7 +4,22 @@ from .models import Item
 # Create your views here.
 
 def item_list(request):
+    
     context = {
         'items': Item.objects.all()
     }
     return render(request, "index.html", context)
+
+
+def fashion(request):
+    return render(request, 'fashion.html')
+
+def electronics(request):
+    return render(request, 'electronic.html')
+
+def jewellery(request):
+    return render(request, 'jewellery.html')
+
+
+
+
